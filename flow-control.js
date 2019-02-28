a82661aba175598fada49b9283970665be0da7cf
Billy Russell <williamnrussell@gmail.com>
2019-02-28 14:13:49 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,14 +3,14 @@ function basicTeenager(age) {
 }
 function teenager(age) {
   if (age >= 13 && age <=19) {
-    return '"You are a teenager!""'
+    return '"You are a teenager!'
   }
   return '"You are not a teenager"'
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <=19) {
-    return '"You are a teenager!""'
+    return '"You are a teenager!'
   } else if (age < 13) {
     return `"You are a kid"`
   } else if (age > 19) {
